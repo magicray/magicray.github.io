87a5537109dc2bb7ad4b733020e468b88815c404
Bhupendra Singh <bhsingh@gmail.com>
2026-02-27 08:02:24 +0530

    simplified screener - removed 3/5 years data. Considering only last 4 quarters

--- rank.py
+++ rank.py
@@ -5,34 +5,22 @@ import time
 import requests
 from logging import critical as log
 
-# OPM > 0 AND
-# OPM 5Year > 0 AND
-#
-# Return on equity > 0 AND
-# Average return on equity 3Years > 0 AND
-# Average return on equity 5Years > 0 AND
-#
-# Return on capital employed > 0 AND
-# Average return on capital employed 3Years > 0 AND
-# Average return on capital employed 5Years > 0 AND
-#
-# Sales growth > 0 AND
-# Sales growth 3Years > 0 AND
-# Sales growth 5Years > 0 AND
-#
-# Profit growth > 0 AND
-# Profit growth 3Years > 0 AND
-# Profit growth 5Years > 0 AND
-#
-# EPS growth 3Years > 0 AND
-# EPS growth 5Years > 0 AND
-#
-# Operating profit growth > 0 AND
-#
-# Operating profit > Interest AND
-# Operating profit > Net profit AND
-#
-# Last result date > 202508
+
+SCREENER = """
+OPM > 0 AND
+Return on equity > 0 AND
+Return on capital employed > 0 AND
+
+Sales growth > 0 AND
+Profit growth > 0 AND
+Operating profit growth > 0 AND
+
+Interest Coverage Ratio > 0 AND
+Operating profit > Interest AND
+Operating profit > Net profit AND
+
+Last result date > 202508
+"""
 
 
 #growth_screen = ('879125/growth', 'chrdjfpkcw07dhqo4nwfh0yd7g191lxl')
